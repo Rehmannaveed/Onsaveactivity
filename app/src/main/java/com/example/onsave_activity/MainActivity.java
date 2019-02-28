cd52d321fe157835ff74fc805d1431ede146e90b
Rehmannaveed <naveedafridi1999@gamil.com>
2019-02-28 23:40:29 +0500

    Initial commit

---
+++ app/src/main/java/com/example/onsave_activity/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.onsave_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btncounter,btntoast;
+    EditText edtext;
+    int counter ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btntoast = findViewById(R.id.btntoast);
+        btncounter = findViewById(R.id.btncounter);
+        edtext = findViewById(R.id.edtext);
+
+
+
+        btncounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+
+            }
+        });
+        btntoast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"you count is " + counter, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("", counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        savedInstanceState.getInt("" ,counter);
+    }
+}
